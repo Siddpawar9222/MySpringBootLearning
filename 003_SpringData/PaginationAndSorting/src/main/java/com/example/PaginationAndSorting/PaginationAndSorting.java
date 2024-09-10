9cc4781d91666d94859eee69a3c87901f33c83df
Siddhesh <siddpawar583@gmail.com>
2024-09-10 22:22:11 +0530

    Working on frontend to implement SAP

--- 003_SpringData/PaginationAndSorting/src/main/java/com/example/PaginationAndSorting/PaginationAndSorting.java
+++ 003_SpringData/PaginationAndSorting/src/main/java/com/example/PaginationAndSorting/PaginationAndSorting.java
@@ -1,5 +1,5 @@
 package com.example.PaginationAndSorting;
-
+import  java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +8,44 @@ public class PaginationAndSorting {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaginationAndSorting.class, args);
+	}
+
+	public int robotSim(int[] commands, int[][] obstacles) {
+        int [][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
+		                     //  N   , E   , S   ,   W
+
+		int dir = 0 ;  // Start with n
+
+		int x =0;
+		int y=0 ;
+		int maxDistance =0 ;
+
+		Set<String>  setObstacles = new HashSet<>();
+		for(int[] row : obstacles){
+			 setObstacles.add(row[0] + " " +row[1]);
+		}
+
+		for(int command : commands){
+			if(command==-1){
+				 dir = (dir + 1) % 4 ;
+			}else if (command==-2){
+				dir = (dir + 3) % 4 ;
+			}else{
+				 for (int i=1 ;i<=command;i++){
+					 int newX = x + directions[dir][0];
+					 int newY = y + directions[dir][1];
+
+					 if(setObstacles.contains(newX+ " "+ newY)){
+						 break;
+					 }
+					 x = newX;
+					 y = newY;
+				 }
+
+				 maxDistance = Math.max(maxDistance,(x*x + y*y));
+			}
+		}
+
+		return maxDistance ;
 	}
 }
--- 003_SpringData/PaginationAndSorting/src/main/java/com/example/PaginationAndSorting/controller/ManufacturerController.java
+++ 003_SpringData/PaginationAndSorting/src/main/java/com/example/PaginationAndSorting/controller/ManufacturerController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
+
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/supplier/")
 @RequiredArgsConstructor
