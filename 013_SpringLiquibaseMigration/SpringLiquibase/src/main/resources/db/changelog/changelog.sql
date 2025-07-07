ccd564c5b55d4b80a451f308be0290a56881811b
siddhesh <siddpawar583@gmail.com>
2025-07-07 18:59:14 +0530

    added spring audit project

--- 013_SpringLiquibaseMigration/SpringLiquibase/src/main/resources/db/changelog/changelog.sql
+++
@@ -1,4 +0,0 @@
--- sql formatted liquibase
-
--- changeset siddhesh:002
-ALTER TABLE spring_liquibase_student ADD COLUMN is_dance BOOLEAN DEFAULT TRUE;
---
+++ 013_SpringLiquibaseMigration/SpringLiquibase/src/main/resources/db/changelog/changes/002-alter-table2.sql
@@ -0,0 +1,7 @@
+-- liquibase formatted sql
+
+-- changeset siddhesh:002
+ALTER TABLE spring_liquibase_student ADD COLUMN is_dance1 BOOLEAN DEFAULT TRUE;
+
+
+
